7c4e33a2261a5378a416a69732d79f500f019038
Marshall Anderson <Anderson_Marshall@shure.com>
2020-11-18 19:51:23 -0600

    Empty project

---
+++ IMDbSearch/Shared/IMDbSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMDbSearchApp.swift
+//  Shared
+//
+//  Created by Anderson, Marshall on 11/18/20.
+//
+
+import SwiftUI
+
+@main
+struct IMDbSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
